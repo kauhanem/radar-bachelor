5e6058a60daeefa8a7b5ce8e61777ddecb2d7790
Mikko Kauhanen <kauhanenmikko@gmail.com>
2017-11-01 12:35:43 +0200

    Added compareTool
    Added compareTool, which returns a vector containing the distances between the input matrices.

--- compareTool.m
+++ compareTool.m
@@ -1,5 +1,5 @@
-function [] = compareAndVisualize(P,Q,D)
-    % COMPAREANDVISUALIZE Compare and visualize RCS histograms.
+function [distances] = compareTool(P,Q,D)
+    % COMPARE Compare and visualize RCS histograms.
     %
     % compareAndVisualize(P,Q) calculates the statistical distance using
     % total variation distance between two discrete probability
@@ -26,39 +26,16 @@ function [] = compareAndVisualize(P,Q,D)
     switch D
         case {'t','T','total','tvd','totalVariationDistance'}
             fun = @totalVariationDistance;
-            titleText = 'Total variation distance';
             
         case {'b','B','bhat','bhattacharyya'}
             fun = @bhattacharyya;
-            titleText = 'Bhattacharyya distance';
             
         case {'h','H','hell','hellinger'}
             fun = @hellinger;
-            titleText = 'Hellinger distance';
             
         otherwise
             error('Error: Distance parameter not known.');
     end
-    
-    figure();
-    
-    % Plot histogram matrix P
-    subplot(3,1,1);
-    
-    imagesc(P);
-    colormap('jet');
-    set(gca,'YDir','normal');
-    
-    title('P');
-    
-    % Plot histogram matrix Q
-    subplot(3,1,2);
-    
-    imagesc(Q);
-    colormap('jet');
-    set(gca,'YDir','normal');
-    
-    title('Q');
     
     % Calculate the distances
     distances = zeros(1, size(P,2));
@@ -66,24 +43,4 @@ function [] = compareAndVisualize(P,Q,D)
     for i = 1:size(P,2)
         distances(i) = fun(P(:,i), Q(:,i));
     end
-    
-    % Plot the distances
-    subplot(3,1,3);
-    
-    stairs(-180 : 360/size(P,2) : 179.999999, distances);
-    hold on;
-    plot([-180 180], [mean(distances) mean(distances)]); % Plots mean value
-    
-    title(titleText);
-    ylabel('Distance');
-    xlabel('Azimuth ({\circ})');
-    
-    % Scale axis
-    if max(distances) >= 0.8
-        maxY = 1.25 * max(distances);
-    else
-        maxY = 1;
-    end
-    
-    axis([-180 180 0 maxY]);
 end
