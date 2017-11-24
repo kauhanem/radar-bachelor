2f57e0b3fabc892cdad1c5132e350eb763dcb5ce
Mikko Kauhanen <kauhanenmikko@gmail.com>
2017-11-24 12:37:32 +0200

    Small changes
    Small changes to compareTool and crossCompare.

--- totalVariationDistance.m
+++ totalVariationDistance.m
@@ -3,6 +3,9 @@ function [distance] = totalVariationDistance(P,Q)
     %
     % totalVariationDistance(P,Q) is the total variation distance between
     % two discrete probability distributions P and Q.
+    %
+    % Mikko Kauhanen
+    % mikko.kauhanen@student.tut.fi
     
     distance = sum(abs(P-Q))/2;
 end
